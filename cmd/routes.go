8e31a5335343c269e3385ecaffcc1fad1ad026e3
Kai Cho <kai.cho@netlogix.co.nz>
2023-11-14 21:45:52 +1300

    handler

---
+++ cmd/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/kai-happyvirus/go-docker-postgres/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.ListFacts)
+
+	app.Post("/fact", handlers.CreateFact)
+}
